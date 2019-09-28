2528371e1105dcfd4890992c270fb6935c6f99ef
Duncan Macleod <duncan.macleod@ligo.org>
2019-09-28 11:15:40 +0100

    updated recipe

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,14 +1,25 @@
 #!/bin/bash
 
-./configure \
-  --prefix=${PREFIX} \
-  --disable-warnings-as-errors \
-  --with-optimization=high \
-  --without-doxygen \
-  --without-dot
+mkdir -p _build
+pushd _build
+
+# link librt to get clock_gettime on older glibc versions
+if [ "$(uname)" == "Linux" ]; then
+	export LDFLAGS="-lrt ${LDFLAGS}"
+fi
+
+# configure
+cmake ${SRC_DIR} \
+	-DCMAKE_INSTALL_PREFIX=${PREFIX} \
+	-DCMAKE_BUILD_TYPE=RelWithDebInfo \
+	-DCMAKE_INSTALL_LIBDIR="lib" \
+	-DCMAKE_DISABLE_FIND_PACKAGE_Doxygen=true
 
 # build
-make -j ${CPU_COUNT}
+cmake --build . --parallel ${CPU_COUNT}
+
+# test
+ctest -V
 
 # install
-make -j ${CPU_COUNT} install
+cmake --build . --target install
